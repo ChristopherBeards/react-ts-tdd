1dabcb41902f986ccf5be9742254bc4cb067e768
Christopher Beards <BeardsCM@gmail.com>
2018-12-19 16:44:29 -0500

    Enzyme finally working

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
+// Test Setup 
+import '../setupTests';
+
+// Component
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the heading', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toBe('Hello World!');
+  });
+})
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  render() {
+  public render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
