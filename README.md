9c13e764d3b0dd635750a8c66866347ab79dfeb5
Christopher Beards <BeardsCM@gmail.com>
2018-12-19 14:33:18 -0500

    Updated

---
+++ README.md
@@ -0,0 +1,6 @@
+# Intro
+
+A simple React app built with TypeScript and TDD.
+
+# Author
+[Christopher Beards](https://www.linkedin.com/in/christopher-beards-1292b529/);
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
